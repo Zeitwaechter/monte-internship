988e562d314df9671b5fa81e7ba2bf9f171567d4
Monty Python <monty.schrey@gmail.com>
2019-01-21 11:09:34 +0100

    Further deep diving into Ionic 4b

--- project-3/src/pages/home/home.ts
+++ project-3/src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PostsPage } from '../posts/posts';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+    //
+  }
 
+  onLoadPost() {
+    this.navCtrl.push(PostsPage);
   }
 
 }
