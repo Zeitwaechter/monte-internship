d7b250d2f523445ab91edfeb64b912a7c33c6592
Monty Python <monty.schrey@gmail.com>
2019-01-21 14:39:17 +0100

    Turorial No. 1 Finish

--- project-3/src/pages/posts/posts.ts
+++ project-3/src/pages/posts/posts.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { PlacesService } from '../../services/places.service';
+
 @IonicPage()
 @Component({
   selector: 'page-posts',
@@ -8,10 +10,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PostsPage {
 
-  constructor(private placesService: PlacesService) {}
+  constructor(private placesService: PlacesService, private navCtrl: NavController) {}
 
   onAddPlace(value: {title: string}){
     this.placesService.addPlace(value);
+    this.navCtrl.pop();
   }
 
 }
